f13f5dab27c1bcca077a762dd44e681957dc300d
diego <diegofernandez29400@gmail.com>
2021-12-17 22:43:24 +0100

    Añadido de carrusel

--- resources/views/stickerAbierto.blade.php
+++ resources/views/stickerAbierto.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
+@section('css')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@endsection
+
 @section('content')
     <div class="firstPartSticker">
         <div class="sticker" style="background-image: url('{{asset('imagesStored/'.$data['post']['picture'])}}');background-size: cover;">
@@ -28,7 +32,7 @@
     <div class="stickerBody">
         <div class="imagenesSticker">
             @foreach($data['images'] as $image)
-                <img src="{{ asset('imagesStored/'.$image) }}">
+                <img style="cursor: pointer" class="openModal" src="{{ asset('imagesStored/'.$image) }}">
             @endforeach
         </div>
 
@@ -71,10 +75,48 @@
             </form>
         </div>
     </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('imagesStored/'.$data['images'][0]) }}" alt="First slide">
+                            </div>
+                            @for($i=1;$i<sizeof($data['images']);$i++)
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="{{asset('imagesStored/'.$data['images'][$i])}}" alt="Third slide">
+                                </div>
+                            @endfor
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('js')
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script>
+        $('.openModal').click(function (e) {
+            $("#exampleModal").modal("show");
+        });
+
         $('#enviarOpinion').click(function (e) {
             if($('#puntuationOpinion').val() != '' && $('#text_opinion').val().length >= 45) {
                 $('#textOpinion').val($('#text_opinion').val())
